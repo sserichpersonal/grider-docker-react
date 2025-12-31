2126471a5187192aa75e05e1fa6c450a2d37d8bf
Scott Serich <scottserich@gmail.com>
2025-12-31 11:50:55 -0500

    new repo feature branch

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          App.js sts edits 20251231 earlier.
+          App.js sts edits 20251231 early (new repo feature branch).
         </p>
         <a
           className="App-link"
