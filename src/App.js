4bdb6a84742be7a8919d4108a583341870a79271
Scott Serich <scottserich@gmail.com>
2025-12-31 11:41:58 -0500

    confirm deploy to Frontend2-env is repeatable

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          App.js sts edits 20251231 earliest.
+          App.js sts edits 20251231 earlier.
         </p>
         <a
           className="App-link"
